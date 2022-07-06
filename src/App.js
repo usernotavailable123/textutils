352789db9be7844100dac1d26ee9a906df73b48f
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-06 15:14:28 +0800

    add about component and its dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
@@ -6,7 +7,8 @@ function App() {
    <>   
       <Navbar title="TextUtils" aboutText="About Text"/>
       <div className='container'>
-        <TextForm heading="Enter the text to analyze below"/>
+        {/* <TextForm heading="Enter the text to analyze below"/> */}
+        <About />
       </div>
    </>
   );
