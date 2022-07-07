a3f50705ec8bc08258cff726869124d1c0695a84
usernotavailable123 <projectpet69@gmail.com>
2022-07-07 15:58:05 +0800

    add dark and light screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,6 +24,9 @@ export default function Navbar(props) {
           <div className="bg-danger rounded mx-2" onClick = {() =>{props.toggleMode('danger')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
           <div className="bg-success rounded mx-2" onClick = {() =>{props.toggleMode('success')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
           <div className="bg-warning rounded mx-2" onClick = {() =>{props.toggleMode('warning')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="bg-dark rounded mx-2" onClick = {() =>{props.toggleMode('dark')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="bg-light rounded mx-2" onClick = {() =>{props.toggleMode('light')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+
         </div>
         {/* <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
             <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
